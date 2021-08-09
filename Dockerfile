e9a5267144559f7942beb61082354cd8495fb84a
Trey Stafford <trey.stafford@nsidc.org>
2021-08-09 17:50:38 -0600

    WIP causing segfault but needs work...
    * overviews missing
    * docker-compose not setup/working

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM qgis/qgis:release-3_16 AS qgis-only 
+FROM continuumio/miniconda3:4.10.3
 
 WORKDIR /
 
@@ -7,24 +7,9 @@ RUN apt-get install -y vim
 RUN apt-get install -y wget
 
 COPY ./src ./
+COPY environment.yml ./
 
-CMD ["python3", "src/test_with_cleanup.py"]
+RUN apt-get update
+RUN apt-get install -y libgl1-mesa-glx
 
-
-
-FROM qgis-only AS qgis-plus-conda
-
-RUN wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
-RUN bash ~/miniconda.sh -b -p
-RUN rm ~/miniconda.sh
-
-CMD ["python3", "src/test_with_cleanup.py"]
-
-
-
-FROM qgis-plus-conda as qgis-in-conda-env
-
-COPY ./environment.yml /
-RUN /bin/bash -c "source ~/miniconda3/bin/activate && conda env create -f /environment.yml"
-
-CMD ./src/run_in_conda.sh python src/test_with_cleanup.py
+RUN conda env create -f environment.yml
--- src/segfault_bug.py
+++ src/segfault_bug.py
@@ -24,7 +24,7 @@ def _init_qgis():
 
 
 def trigger_with_vector():
-    vector_path = '/tmp/populated_places.gpkg'
+    vector_path = '/populated_places.gpkg'
     result = qgc.QgsVectorLayer(
         vector_path,
         'vectorlayer',
@@ -34,16 +34,31 @@ def trigger_with_vector():
     return result
 
 
+def trigger_with_raster():
+    raster_path = '/overviews.tif'
+    result = qgc.QgsRasterLayer(
+        raster_path,
+        'overviews',
+        'gdal'
+    )
+
+    return result
+
+
 def main():
     qgs = _init_qgis()
 
     vector_result = trigger_with_vector()
+    raster_result = trigger_with_raster()
     print(vector_result)
+    print(raster_result)
 
     del vector_result
     qgs.exitQgis()
 
 
 def main_without_cleanup():
+    raster_result = trigger_with_raster()
     vector_result = trigger_with_vector()
     print(vector_result)
+    print(raster_result)
